d9465fd7036b2f22116d36d55e6a35c0b8b0bb23
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-02 06:36:20 +0530

    Initial commit: migrated away from mason.nvim to nixpkgs & added support of copilot

---
+++ lua/chadrc.lua
@@ -0,0 +1,54 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+
+return {
+  base46 = {
+    theme = 'gatekeeper',
+    transparency = true,
+    integrations = {
+      "git",
+      "cmp",
+      "blankline",
+    },
+    -- highlights
+    hl_override = {
+      Comment = { italic = true, },
+    },
+    hl_add = {
+      NvimTreeOpenedFolderName = { fg = 'green', bold = true },
+    },
+  },
+
+  ui ={
+    cmp = {
+      icons = true,
+      lspkind_text = true,
+    },
+    nvdash = {
+      load_on_startup = false,
+    },
+    statusline = {
+      separator_style = 'round',
+    },
+  },
+
+  cheatsheet = {
+    theme = 'grid',
+  },
+
+  term = {
+    winopts = {
+      number = true,
+      relativenumber = true,
+    }
+  },
+
+  lsp = {
+    signature = true,
+  },
+
+  -- mason = {
+  --   cmd = true,
+  --   pkgs = require 'configs.mason_pkgs',
+  -- },
+}
