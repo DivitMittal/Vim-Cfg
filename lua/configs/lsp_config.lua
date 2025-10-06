d48e0ad6a738bcd2412990b199c0a271061a163c
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-10-06 19:06:42 +0530

    fix(lsp): modify lsp config for deprecated lspconfig module

--- lua/configs/lsp_config.lua
+++ lua/configs/lsp_config.lua
@@ -1,67 +1,54 @@
-local lspconfig = require "lspconfig"
 local chadlsp = require "nvchad.configs.lspconfig"
-local attach = function(client, bufnr)
-  chadlsp.on_attach(client, bufnr)
-  local map = vim.keymap.set
-  -- lspsaga
-  map("n", "gh", "<cmd>Lspsaga finder<cr>", { buffer = bufnr, desc = "Lspsaga finder" })
-  map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", { buffer = bufnr, desc = "Lspsaga code action" })
-  map("n", "gr", "<cmd>Lspsaga rename<cr>", { buffer = bufnr, desc = "Lspsaga rename" })
-  map("n", "gp", "<cmd>Lspsaga peek_definition<cr>", { buffer = bufnr, desc = "Lspsaga peek definition" })
-  map("n", "gd", "<cmd>Lspsaga goto_definition<cr>", { buffer = bufnr, desc = "Lspsaga goto definition" })
-  map("n", "gt", "<cmd>Lspsaga peek_type_definition<cr>", { buffer = bufnr, desc = "Lspsaga peek type definition" })
-  map(
-    "n",
-    "<leader>dl",
-    "<cmd>Lspsaga show_line_diagnostics<cr>",
-    { buffer = bufnr, desc = "Lspsaga show line diagnostics" }
-  )
-  map(
-    "n",
-    "<leader>dw",
-    "<cmd>Lspsaga show_workspace_diagnostics<cr>",
-    { buffer = bufnr, desc = "Lspsaga show workspace diagnostics" }
-  )
-  map(
-    "n",
-    "<leader>dc",
-    "<cmd>Lspsaga show_cursor_diagnostics<cr>",
-    { buffer = bufnr, desc = "Lspsaga show cursor diagnostics" }
-  )
-  map("n", "<leader>k", "<cmd>Lspsaga hover_doc<cr>", { buffer = bufnr, desc = "Lspsaga hover doc" })
-  map("n", "<leader>ci", "<cmd>Lspsaga incoming_calls<cr>", { buffer = bufnr, desc = "Lspsaga incoming calls" })
-  map("n", "<leader>co", "<cmd>Lspsaga outgoing_calls<cr>", { buffer = bufnr, desc = "Lspsaga outgoing calls" })
-  map("n", "<leader>o", "<cmd>Lspsaga outline<cr>", { buffer = bufnr, desc = "Lspsaga outline" })
-end
+local map = vim.keymap.set
 
 chadlsp.defaults()
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local function opts(desc)
+      return { buffer = args.buf, desc = "LSP " .. desc }
+    end
+
+    map("n", "gd", "<cmd>Lspsaga goto_definition<cr>", opts "Lspsaga goto definition")
+    map("n", "gp", "<cmd>Lspsaga peek_definition<cr>", opts "Lspsaga peek definition")
+    map("n", "gt", "<cmd>Lspsaga peek_type_definition<cr>", opts "Lspsaga peek type definition")
+    map("n", "<leader>k", "<cmd>Lspsaga hover_doc<cr>", opts "Lspsaga hover doc")
+
+    map("n", "gh", "<cmd>Lspsaga finder<cr>", opts "Lspsaga finder")
+    map({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<cr>", opts "Lspsaga code action")
+    map("n", "gr", "<cmd>Lspsaga rename<cr>", opts "Lspsaga rename")
+
+    map("n", "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<cr>", opts "Lspsaga show line diagnostics")
+    map("n", "<leader>dw", "<cmd>Lspsaga show_workspace_diagnostics<cr>", opts "Lspsaga show workspace diagnostics")
+    map("n", "<leader>dc", "<cmd>Lspsaga show_cursor_diagnostics<cr>", opts "Lspsaga show cursor diagnostics")
+
+    map("n", "<leader>ci", "<cmd>Lspsaga incoming_calls<cr>", opts "Lspsaga incoming calls")
+    map("n", "<leader>co", "<cmd>Lspsaga outgoing_calls<cr>", opts "Lspsaga outgoing calls")
+
+    map("n", "<leader>o", "<cmd>Lspsaga outline<cr>", opts "Lspsaga outline")
+  end,
+})
+
 local servers = {
   -- vscode-langservers-extracted
   "html",
   "cssls",
   "jsonls",
   "eslint",
-  "svelte",
 
+  "svelte",
+  "ts_ls",
+  "emmet_language_server",
+  "hls",
+  "pylsp",
   "lua_ls",
   "bashls",
   "nixd",
   "clangd",
+  "sourcekit" -- swift
 }
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = attach,
-    on_init = chadlsp.on_init,
-    capabilities = chadlsp.capabilities,
-  }
-end
 
--- html
-lspconfig.emmet_language_server.setup {
-  on_attach = attach,
-  on_init = chadlsp.on_init,
-  capabilities = chadlsp.capabilities,
+vim.lsp.config("emmet_language_server", {
   filetypes = {
     "css",
     "html",
@@ -76,13 +63,9 @@ lspconfig.emmet_language_server.setup {
     "typescriptreact",
     "vue",
   },
-}
+})
 
--- typescript
-lspconfig.ts_ls.setup {
-  on_attach = attach,
-  on_init = chadlsp.on_init,
-  capabilities = chadlsp.capabilities,
+vim.lsp.config("ts_ls", {
   init_options = {
     plugins = {
       {
@@ -104,19 +87,19 @@ lspconfig.ts_ls.setup {
     "typescriptreact",
     "vue",
   },
-}
+})
 
---python
-lspconfig.pylsp.setup {
-  on_attach = attach,
-  on_init = chadlsp.on_init,
-  capabilities = chadlsp.capabilities,
+vim.lsp.config("hls", {
+  filetypes = { "haskell", "lhaskell", "cabal" },
+})
+
+vim.lsp.config("pylsp", {
   configurationSources = { "flake8" },
   settings = {
     plugins = {
       flake8 = {
         enabled = true,
-        maxLineLength = 80,
+        maxLineLength = 120,
       },
       pycodestyle = { enabled = false },
       pyflakes = { enabled = false },
@@ -124,26 +107,6 @@ lspconfig.pylsp.setup {
       mccabe = { enabled = false },
     },
   },
-}
+})
 
--- haskell
-lspconfig.hls.setup {
-  on_attach = attach,
-  on_init = chadlsp.on_init,
-  capabilities = chadlsp.capabilities,
-  filetypes = { "haskell", "lhaskell", "cabal" },
-}
-
--- markdown-oxide
--- lspconfig.markdown_oxide.setup {
---   on_attach = attach,
---   on_init = chadlsp.on_init,
---   capabilities = vim.tbl_deep_extend("force", chadlsp.capabilities, {
---     workspace = {
---       didChangeWatchedFiles = {
---         dynamicRegistration = true,
---       },
---     },
---   }),
---   filetypes = { "markdown", "vimwiki" },
--- }
+vim.lsp.enable(servers)
