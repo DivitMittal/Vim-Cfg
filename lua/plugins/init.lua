2cd7a876e043a12d1ebf1c1f610aa3855b07be26
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-10-29 12:10:04 +0530

    feat(markdown): enhance rendering with custom styles
    Configures `render-markdown.nvim` with custom heading options,
    a pipe table preset, and specific highlight groups for headings,
    bullets, checkboxes, and table elements for improved visual clarity.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -436,6 +436,45 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
     opts = {
       file_types = { "markdown", "Avante", "vimwiki" },
+
+      heading = {
+        icons = { "◉ ", "◎ ", "◆ ", "◇ ", "● ", "○ " },
+        left_margin = { 0, 1, 2, 3, 4, 5 },
+        min_width = { 100, 70, 60, 50, 40, 30 },
+        border = { true, false, false, false, false, false },
+        above = "═",
+        below = "═",
+      },
+
+      pipe_table = {
+        preset = "round",
+      },
+
+      on = {
+        attach = function()
+          vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = "#89DDFF", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { bg = "#1A2730", fg = "#89DDFF" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH1Border", { fg = "#BD93F9", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = "#FFB86C", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { bg = "#2A2318", fg = "#FFB86C" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = "#F1FA8C", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { bg = "#252619", fg = "#F1FA8C" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH4", { fg = "#8AFF80" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { bg = "#1A2518", fg = "#8AFF80" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH5", { fg = "#6AD6D6" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { bg = "#182525", fg = "#6AD6D6" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH6", { fg = "#8896D6" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { bg = "#1A1D2B", fg = "#8896D6" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownInlineHighlight", { fg = "#FF79C6", bg = "#2E1A26" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownBullet", { fg = "#50D0FF", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownUnchecked", { fg = "#FF6B6B", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownChecked", { fg = "#50FA7B", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownTodo", { fg = "#FFB86C", bold = true })
+          vim.api.nvim_set_hl(0, "RenderMarkdownTableHead", { fg = "#BD93F9", bold = true, bg = "#1A1825" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownTableRow", { fg = "#6AD6D6" })
+          vim.api.nvim_set_hl(0, "RenderMarkdownTableFill", { fg = "#44475A" })
+        end,
+      },
     },
   },
 
@@ -505,72 +544,72 @@ return {
   --                Custom Disabled Plugins
   -- ----------------------------------------------------------- --
   -- Automated Markdown bullets
-  {
-    "bullets-vim/bullets.vim",
-    enabled = false,
-    cond = not isVSCode,
-    lazy = true,
-    event = "BufEnter",
-    init = function()
-      vim.g.bullets_enabled_filetypes = { "markdown", "text", "gitcommit" }
-      vim.g.bullets_enable_in_empty_buffers = 1
-    end,
-  },
-
-  -- Copilot Chat Interface
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    enabled = false,
-    cond = not isVSCode,
-    lazy = true,
-    cmd = { "CopilotChat" },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    dependencies = {
-      "zbirenbaum/copilot.lua",
-      "nvim-lua/plenary.nvim", -- for curl, log and async functions
-    },
-    opts = {},
-  },
-
-  -- multicursors.nvim & hydra.nvim(custom keybinding creation)
-  {
-    "smoka7/multicursors.nvim",
-    enabled = false,
-    cond = not isVSCode,
-    lazy = true,
-    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-    dependencies = { "smoka7/hydra.nvim" },
-    opts = {},
-    keys = {
-      {
-        mode = { "v", "n" },
-        "<leader>mc",
-        "<cmd>MCstart<cr>",
-        desc = "selected word under the cursor and listen for actions",
-      },
-    },
-  },
-
-  -- Linting
-  {
-    "mfussenegger/nvim-lint",
-    enabled = false,
-    cond = not isVSCode,
-    lazy = true,
-    event = { "BufWritePost", "BufReadPost", "InsertLeave" },
-    opts = function(_, _)
-      return require "configs.lint_opts"
-    end,
-    config = function(_, opts)
-      local lint = require "lint"
-      lint.linters_by_ft = opts
-      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-        group = vim.api.nvim_create_augroup("lint", { clear = true }),
-        pattern = "*",
-        callback = function()
-          lint.try_lint()
-        end,
-      })
-    end,
-  },
+  -- {
+  --   "bullets-vim/bullets.vim",
+  --   enabled = false,
+  --   cond = not isVSCode,
+  --   lazy = true,
+  --   event = "BufEnter",
+  --   init = function()
+  --     vim.g.bullets_enabled_filetypes = { "markdown", "text", "gitcommit" }
+  --     vim.g.bullets_enable_in_empty_buffers = 1
+  --   end,
+  -- },
+  --
+  -- -- Copilot Chat Interface
+  -- {
+  --   "CopilotC-Nvim/CopilotChat.nvim",
+  --   enabled = false,
+  --   cond = not isVSCode,
+  --   lazy = true,
+  --   cmd = { "CopilotChat" },
+  --   build = "make tiktoken", -- Only on MacOS or Linux
+  --   dependencies = {
+  --     "zbirenbaum/copilot.lua",
+  --     "nvim-lua/plenary.nvim", -- for curl, log and async functions
+  --   },
+  --   opts = {},
+  -- },
+  --
+  -- -- multicursors.nvim & hydra.nvim(custom keybinding creation)
+  -- {
+  --   "smoka7/multicursors.nvim",
+  --   enabled = false,
+  --   cond = not isVSCode,
+  --   lazy = true,
+  --   cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+  --   dependencies = { "smoka7/hydra.nvim" },
+  --   opts = {},
+  --   keys = {
+  --     {
+  --       mode = { "v", "n" },
+  --       "<leader>mc",
+  --       "<cmd>MCstart<cr>",
+  --       desc = "selected word under the cursor and listen for actions",
+  --     },
+  --   },
+  -- },
+  --
+  -- -- Linting
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   enabled = false,
+  --   cond = not isVSCode,
+  --   lazy = true,
+  --   event = { "BufWritePost", "BufReadPost", "InsertLeave" },
+  --   opts = function(_, _)
+  --     return require "configs.lint_opts"
+  --   end,
+  --   config = function(_, opts)
+  --     local lint = require "lint"
+  --     lint.linters_by_ft = opts
+  --     vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+  --       group = vim.api.nvim_create_augroup("lint", { clear = true }),
+  --       pattern = "*",
+  --       callback = function()
+  --         lint.try_lint()
+  --       end,
+  --     })
+  --   end,
+  -- },
 }
